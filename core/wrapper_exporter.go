400e96b2b397974d3804ade34cb3fbc4a9b67cb2
Florent Colinet <23095517+FColinet@users.noreply.github.com>
2026-02-23 00:07:11 +0000

    Debug: RPC

--- core/wrapper_exporter.go
+++ core/wrapper_exporter.go
@@ -6,24 +6,20 @@ import (
 	"github.com/hashicorp/go-plugin"
 )
 
+// 1. Le Client RPC (Core -> Plugin)
 type ExporterRPCClient struct{ client *rpc.Client }
 
-func (m *ExporterRPCClient) Init(config map[string]string) error {
-	var resp error
-	// On appelle "Plugin.Init" sur le binaire distant
-	err := m.client.Call("Plugin.Init", config, &resp)
-	if err != nil {
-		return err
-	}
-	return resp
+func (g *ExporterRPCClient) Init(config map[string]string) error {
+	var resp struct{}
+	return g.client.Call("Plugin.Init", config, &resp)
 }
 
 func (g *ExporterRPCClient) Export(record WeatherRecord) error {
-	var resp struct{}
-	err := g.client.Call("Plugin.Export", record, &resp)
-	return err
+	var resp struct{} // Obligatoire pour net/rpc même si on ne s'en sert pas
+	return g.client.Call("Plugin.Export", record, &resp)
 }
 
+// 2. Le Serveur RPC (Plugin -> Core)
 type ExporterRPCServer struct{ Impl Exporter }
 
 func (s *ExporterRPCServer) Init(config map[string]string, resp *struct{}) error {
@@ -34,6 +30,7 @@ func (s *ExporterRPCServer) Export(record WeatherRecord, resp *struct{}) error {
 	return s.Impl.Export(record)
 }
 
+// 3. Le Plugin
 type ExporterPlugin struct {
 	Impl Exporter
 }
